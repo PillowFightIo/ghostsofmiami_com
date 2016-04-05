1c5b3b29524f411ce32b44fd56e1fcb2d8b95b77
Kim Nguyen <design@kimisgold.com>
2016-04-05 12:48:29 -0400

    Check in first files.

---
+++ moodboard/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Ghosts of Miami moodboard</title>
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,300|Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="../css/moodboard.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div id="colors">
+    <div class="primary color"></div>
+    <div class="secondary color"></div>
+    <div class="tertiary color"></div>
+</div>
+
+<div id="typography">
+    <p>Heading 1 <span class="heading-1">Vevey</span></p>
+    <p>Heading 2 <span class="heading-2">Oswald</span></p>
+    <p>Heading 3 <span class="heading-3">Vevey</span></p>
+
+    <div class="body-copy">
+        Body Copy
+        <p>Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.</p>
+
+        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+
+        <p>Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget urna mollis ornare vel eu leo. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+    </div>
+</div>
+
+<div id="imagery">
+    <div class="pattern"></div>
+    <div class="type"><span>Bienvenido</span></div>
+    <div class="pattern"></div>
+    <div class="screenshots">
+        <div class="shadow"><img src="../img/screenshot-1.png"></div>
+        <div class="shadow"><img src="../img/screenshot-2.jpg"></div>
+    </div>
+</div>
+
+</body>
+
+</html>
---
+++ wireframes/index.php
@@ -0,0 +1,192 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Ghosts of Miami</title>
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="../css/wireframes.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<header>
+    <h1>Ghosts of Miami</h1>
+    <button type="button" class="language">Espa&ntilde;ol</button>
+    <button type="button" class="pause-toggle fa fa-play-circle-o">Play</button>
+</header>
+
+<div id="content">
+    <h1>Ghosts of Miami</h1>
+
+    <a href="#latest" class="dev-blog">Read the latest from the devs</a>
+
+    <div id="intro" class="section">
+        <div class="container">
+            <p><span class="drop-caps">Miami, 1986</span> On average, one person is murdered every day in Dade County. Between the drug cartels, the AIDS epidemic, and a flood of undocumented immigrants, hundreds more will vanish, never to be seen again. Chelo Mart&iacute;nez finds the people who slip through the cracks. </p>
+
+            <p>Help Chelo explore the beaches and back alleys of Miami at its most dangerous, investigating her very first cases as a private investigator. You make the decisions: do you bait the shady drug dealer, or beat the answers out of him? When that handsome stranger shows up at Chelo&rsquo;s door, will he help your case--or distract you from it? And will you be able to find Chelo&rsquo;s missing father, who disappeared without a word almost ten years ago? </p>
+
+            <p>GHOSTS OF MIAMI features crimes you can&rsquo;t solve, romances that could wreck you, and 80s-inspired art and music that will keep you coming back. At launch, it will be playable in English, Spanish, and Traditional Chinese.</p>
+
+            <div class="vendors">Coming 2016 to<br>
+                <div class="logo">Steam</div>
+                <div class="logo">iOS</div>
+                <div class="logo">Google Play</div>
+                <div class="logo">Itch.io</div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="latest" class="section">
+        <div class="container">
+        <h2>Updates</h2>
+        <h3>Post Title</h3>
+        <span class="date">3.9.2016</span>
+        <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        <a href="#" class="jump fa fa-caret-right">Read more on the dev blog</a>
+        </div>
+    </div>
+
+    <div id="characters" class="section">
+        <div class="container">
+            <div class="character">
+                <div id="chelo" class="portrait"></div>
+                <div class="profile">
+                    <h3>Consuelo &ldquo;Chelo&rdquo; Mart&iacute;nez</h3>
+                    <div class="attribute"><span class="label">Age:</span> 23</div>
+                    <div class="attribute"><span class="label">Occupation:</span> Finding Missing People</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> Finding Tasty Snacks</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> To help her father and others who&rsquo;ve disappeared to reunite with their families. </div>
+                    <div class="quote">&ldquo;No one just up and disappears, right&hellip;?&rdquo;</div>
+                </div>
+            </div>
+
+            <div class="character">
+                <div id="luci" class="portrait"></div>
+                <div class="profile">
+                    <h3>Luciana &ldquo;Luci&rdquo; Escobar</h3>
+                    <div class="attribute"><span class="label">Age:</span> 24</div>
+                    <div class="attribute"><span class="label">Occupation:</span> VIP</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> The perfect stilettos</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> To live up to the Escobar name and reputation.</div>
+                    <div class="quote">&ldquo;No, I would not betray your lovely face for even the handsomest spy.&rdquo;</div>
+                </div>
+            </div>
+
+            <div class="character">
+                <div id="ethan" class="portrait"></div>
+                <div class="profile">
+                    <h3>Ethan Brown</h3>
+                    <div class="attribute"><span class="label">Age:</span> 21</div>
+                    <div class="attribute"><span class="label">Occupation:</span> EMT</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> Being good to his momma.</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> To someday put himself through nursing school, so he can support a family of his own.</div>
+
+                    <div class="quote">&ldquo;I&rsquo;m ready to help! Just&hellip; tell me what I can do.&rdquo;</div>
+                </div>
+            </div>
+
+            <div class="character">
+                <div id="tala" class="portrait"></div>
+                <div class="profile">
+                    <h3>Tala Nassiri</h3>
+                    <div class="attribute"><span class="label">Age:</span> 18</div>
+                    <div class="attribute"><span class="label">Occupation:</span> High School Student</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> Chelo&rsquo;s old high school band</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> To be in a popular movie so that maybe her father, a political prisoner in Iran, can see her.</div>
+                    <div class="quote">&ldquo;Have you seen the latest episode of Miami Vice? We can&rsquo;t talk until you have.&rdquo;</div>
+                </div>
+            </div>
+
+            <div class="character">
+                <div id="janet" class="portrait"></div>
+                <div class="profile">
+                    <h3>Janet Lessig</h3>
+                    <div class="attribute"><span class="label">Age:</span> 30</div>
+                    <div class="attribute"><span class="label">Occupation:</span> Professional Drag King</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> The perfect hair swoop</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> Just to take care of herself and others like her.</div>
+                    <div class="quote">&ldquo;I&rsquo;m the Prince Charming you&rsquo;ve been looking for&mdash;you&rsquo;ve just been looking in the wrong direction.&rdquo;</div>
+                </div>
+            </div>
+
+            <div class="character">
+                <div id="richard" class="portrait"></div>
+                <div class="profile">
+                <h3>&ldquo;Richard&rdquo;</h3>
+                    <div class="attribute"><span class="label">Age:</span> 52</div>
+                    <div class="attribute"><span class="label">Occupation:</span> Gentleman Gangster</div>
+                    <div class="attribute"><span class="label">Preoccupation:</span> Being mysterious</div>
+                    <div class="attribute"><span class="label">Life Goal:</span> ???</div>
+                    <div class="quote">&ldquo;You may never be ready for me. But when you are, call.&rdquo;</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="team" class="section">
+        <div class="container">
+            <div class="credit">
+                <h3>Writer & Producer</h3>
+                <span class="name">Jo Fu</span>
+            </div>
+            <div class="credit">
+                <h3>Creative Director</h3>
+                <span class="name">Conrad Kreyling</span>
+            </div>
+            <div class="credit">
+                <h3>Sprite Artist</h3>
+                <span class="name">Cassie Freire</span>
+            </div>
+            <div class="credit">
+                <h3>UX Artist & Programmer</h3>
+                <span class="name">Kim Nguyen</span>
+            </div>
+            <div class="credit">
+                <h3>Story Consultants/Editors</h3>
+                <span class="nane">Stella Ramirez</span>
+                <span class="name">Iasmin Omar Ata</span>
+                <span class="name">Ryan Murphy</span>
+            </div>
+            <div class="credit">
+                <h3>Music</h3>
+                <span class="name">Casey Yates</span>
+                <span class="name">Christopher D. Russell</span>
+            </div>
+            <p>GHOSTS OF MIAMI is presented by PILLOWFIGHT GAMES, from the dev team that brought you WE KNOW THE DEVIL.</p>
+        </div>
+    </div>
+
+</div>
+
+<script type="text/javascript">
+(function($) {
+    $(document).ready(function() {
+        var mainTitle = $('#content h1');
+        var mainTitleHeight = mainTitle.css('height');
+        mainTitleHeight = mainTitleHeight.substr(0, mainTitleHeight.length -2);
+        var headerTitle = $('header h1');
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > (mainTitleHeight/2)) {
+                headerTitle.addClass('visible');
+            } else {
+                headerTitle.removeClass('visible');
+            }
+        });
+
+        $('.pause-toggle').click(function() {
+            $(this).toggleClass('fa-play-circle-o');
+            $(this).toggleClass('fa-pause-circle-o');
+        });
+    });
+})(jQuery)
+
+</script>
+
+</body>
+</html>
